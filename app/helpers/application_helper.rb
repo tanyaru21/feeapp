550b74980b0e8a637e1208ed00c2511039d21ecf
tanyaru21 <tanya.ru21@gmail.com>
2019-03-26 15:15:58 +0000

    full title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+#returns a full title on a per page basis
+
+    def full_title(page_title = '')
+        base_title = "Fee Application"
+        if page_title.empty?
+            base_title
+        else
+            page_title + ' | ' + base_title
+        end
+    end
 end
